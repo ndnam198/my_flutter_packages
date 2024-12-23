face752eacc78431141543c2d197e49e3a4d748d
ndnam198 <ndnam198@gmail.com>
2024-12-23 16:49:00 +0700

    add test case to inscrease coverage

--- detached_scrollbar/test/src/detached_scrollbar_test.dart
+++ detached_scrollbar/test/src/detached_scrollbar_test.dart
@@ -22,7 +22,7 @@ void main() {
     expect(find.byType(DetachedScrollBar), findsOneWidget);
   });
 
-  testWidgets('DetachedScrollBar updates scroll position on handle dragged', (WidgetTester tester) async {
+  testWidgets('DetachedScrollBar updates scroll position on handle dragged (vertically)', (WidgetTester tester) async {
     final scrollController = ScrollController();
     final scrollableContent = List.generate(100, (index) => ListTile(title: Text('Item $index')));
     final scrollableWidget = ListView.builder(
@@ -52,6 +52,48 @@ void main() {
 
     expect(scrollController.offset, equals(0));
     await tester.drag(handle, Offset(0, 50));
+    await tester.pumpAndSettle();
+    expect(scrollController.offset, greaterThan(0));
+  });
+
+  testWidgets('DetachedScrollBar updates scroll position on handle dragged (horizontal case)',
+      (WidgetTester tester) async {
+    final scrollController = ScrollController();
+    final scrollableContent = List.generate(
+      100,
+      (index) => Container(
+        width: 100,
+        color: index.isEven ? Colors.blue : Colors.red,
+        child: Center(child: Text('Item $index')),
+      ),
+    );
+    final scrollableWidget = ListView.builder(
+      controller: scrollController,
+      itemCount: scrollableContent.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) => scrollableContent[index],
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Stack(
+            children: [
+              scrollableWidget,
+              DetachedScrollBar(
+                scrollController: scrollController,
+                trackLength: 200,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    final handle = find.byKey(handleKey).first;
+
+    expect(scrollController.offset, equals(0));
+    await tester.drag(handle, Offset(50, 0)); // Drag horizontally
     await tester.pumpAndSettle();
     expect(scrollController.offset, greaterThan(0));
   });
@@ -191,5 +233,52 @@ void main() {
     );
 
     expect(find.byKey(handleKey), findsNothing);
+  });
+
+  testWidgets('DetachedScrollBar calls didUpdateWidget when properties change', (WidgetTester tester) async {
+    final scrollController = ScrollController();
+    final scrollableContent = List.generate(
+      100,
+      (index) => ListTile(title: Text('Item $index')),
+    );
+
+    await tester.binding.setSurfaceSize(Size(2000, 2000));
+
+    // Define a StatefulWidget to allow dynamic updates
+    Widget buildTestWidget({required double trackLength}) {
+      return MaterialApp(
+        home: Scaffold(
+          body: Stack(
+            children: [
+              ListView.builder(
+                controller: scrollController,
+                itemCount: scrollableContent.length,
+                itemBuilder: (context, index) => scrollableContent[index],
+              ),
+              DetachedScrollBar(
+                scrollController: scrollController,
+                trackLength: trackLength,
+                isHorizontal: false,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    // Initial widget with trackLength = 200
+    await tester.pumpWidget(buildTestWidget(trackLength: 200));
+
+    // Verify initial state
+    final handle = find.byKey(handleKey);
+    final handleSize1 = tester.getSize(handle.first);
+
+    // Update the widget by changing trackLength
+    await tester.pumpWidget(buildTestWidget(trackLength: 300));
+
+    final handleSize2 = tester.getSize(handle.first);
+
+    // increase track length should shrink handle size
+    expect(handleSize1.longestSide, lessThan(handleSize2.longestSide));
   });
 }
